2e87c1e3b6b756487e51597097edda16e75d6eb5
Josephat Ndungu <josendungu.jm@gmail.com>
2020-01-18 11:11:32 +0300

    adding forgotActivity to change password

--- app/src/main/java/com/example/fetchpost/ForgotActivity.java
+++ app/src/main/java/com/example/fetchpost/ForgotActivity.java
@@ -1,22 +1,34 @@
 package com.example.fetchpost;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class ForgotActivity extends AppCompatActivity {
 
     EditText etUsername, etPass, etPassConf;
     Button btnSubmit;
     String username, pass, passConf;
+    Context mContext;
+    private static final String TAG = "ForgotActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
+
+        mContext = this;
 
         etUsername = (EditText)findViewById(R.id.username);
         etPass = (EditText)findViewById(R.id.password1);
@@ -30,11 +42,44 @@ public class ForgotActivity extends AppCompatActivity {
                 pass = etPass.getText().toString();
                 passConf = etPassConf.getText().toString();
 
-
-
+                if(pass.equals(passConf)){
+                    new forgotTask().execute();
+                } else {
+                    Log.d(TAG, "onClick: Pass don't match");
+                }
             }
         });
 
 
+    }
+
+    public class forgotTask extends AsyncTask<String,String,String>{
+
+        String result;
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            if(result.equals(savedInfo.success)){
+
+            }
+            super.onPostExecute(s);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String data = URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")
+                        +"&&"+URLEncoder.encode("pass","UTF-8")+"="+URLEncoder.encode(pass,"UTF-8");
+                DB_con db = new DB_con(mContext,savedInfo.baseUrl+savedInfo.forgot, data);
+                result = db.getConnection();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/fetchpost/savedInfo.java
+++ app/src/main/java/com/example/fetchpost/savedInfo.java
@@ -2,13 +2,17 @@ package com.example.fetchpost;
 
 public class savedInfo {
 
-    public static String baseUrl = "http://192.168.100.78/fetchpost/";
+    public static String baseUrl = "http://192.168.0.193/fetchpost/";
 
 
     public static String add_member = "register.php";
     public static String login = "login.php";
+    public static String forgot = "forgot.php";
 
-    public static String mifiConnIp = "192.168.0.193";
-    public static String hotConnIp = "192.168.137";
+    public static String success = "1";
+    public static String memDontExist = "2";
+
+
+
 
 }
