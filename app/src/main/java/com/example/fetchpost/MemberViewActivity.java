330f70e5d5982920b20a54f06086cea88aded779
Josephat Ndungu <josendungu.jm@gmail.com>
2020-04-10 20:43:20 +0300

    memberview activity layout

---
+++ app/src/main/java/com/example/fetchpost/MemberViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.fetchpost;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MemberViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_view);
+    }
+}
